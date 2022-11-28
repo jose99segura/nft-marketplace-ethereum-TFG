96807d83733429963074c2e53f55487e5d310dba
jose99segura <jose99segura@gmail.com>
2022-11-28 17:37:53 +0100

    er

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -1,13 +1,29 @@
-import { createContext, FunctionComponent, useContext, useState } from "react";
+import { createContext, FunctionComponent, useContext, useEffect, useState } from "react";
+import { createDefaultState, Web3State } from "./utils";
+import { ethers } from "ethers";
 
 
-
-const Web3Context = createContext(null);
+const Web3Context = createContext<Web3State>(createDefaultState());
 
 
 const Web3Provider: FunctionComponent = ({children}:any) => {
 
-    const [web3Api, setWeb3Api] = useState({test:"Hello provider"})
+    const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState());
+
+    useEffect(() => {
+        function initWeb3(){
+            
+            const provider = new ethers.providers.Web3Provider(window.ethereum as any);
+
+            setWeb3Api({
+                ethereum: window.ethereum,
+                provider,
+                contract: null,
+                isLoading: false
+            })
+        }
+        initWeb3();
+    }, [])
 
     return(
         <Web3Context.Provider value={web3Api}>
