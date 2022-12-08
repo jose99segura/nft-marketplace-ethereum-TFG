dfd960535046017cc624c7bbfd0c34c0f7619363
jose99segura <jose99segura@gmail.com>
2022-12-08 13:33:58 +0100

    Smart Contracts

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -1,7 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NftMarket is ERC721URIStorage{
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _listedItems;      //Cuantos NFTs hay
+    Counters.Counter private _tokenIds;
+
+
     constructor() ERC721("CreaturesNFT", "CNFT"){}
+
+    function mintToken(string memory tokenURI) public payable returns (uint){   
+        //tokenURI es el link al metadata, pinata en nuestro caso --> https://pinata.com/metadata
+        //msg.sender es la dirección de nuestra cartera de metamask
+        _tokenIds.increment();
+        _listedItems.increment();
+
+        uint newTokenId = _tokenIds.current();
+
+        _safeMint(msg.sender, newTokenId);      
+        _setTokenURI(newTokenId, tokenURI);
+
+        return newTokenId;
+    }
+    
 }
