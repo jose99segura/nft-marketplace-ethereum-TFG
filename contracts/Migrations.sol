ed8e31f7f6f6fd8046af8c01377e9220d8935668
jose99segura <jose99segura@gmail.com>
2023-03-02 17:15:35 +0100

    mini traduccion

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -8,7 +8,7 @@ contract Migrations {
   modifier restricted() {
     require(
       msg.sender == owner,
-      "This function is restricted to the contract's owner"
+      "Esta funcion esta restrictida al creador del contrato"
     );
     _;
   }
