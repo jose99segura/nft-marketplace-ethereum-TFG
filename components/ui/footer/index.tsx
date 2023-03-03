f2c4b9742e50d68619aad373a0dd97c363e0c2de
jose99segura <jose99segura@gmail.com>
2023-03-03 15:19:02 +0100

    footer

--- components/ui/footer/index.tsx
+++ components/ui/footer/index.tsx
@@ -2,7 +2,18 @@
 export default function Example() {
   return (
     <>
-    <div>Footer</div>
+    
+    <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 bg-gray-800">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 - Gestión de entradas a traves de una aplicación descentralizada en Blockchain
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <a href="https://github.com/jose99segura/nft-marketplace" className="mr-4 hover:underline md:mr-6 ">jose99segura</a>
+            </li>
+
+        </ul>
+    </footer>
+
     </>
   )
 }
