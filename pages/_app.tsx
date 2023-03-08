adf97868aa88cb292534f5efe65b9b5961f36fe3
jose99segura <jose99segura@gmail.com>
2023-03-08 11:00:46 +0100

    public actualizado

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Web3Provider } from '@providers'
+import Head from 'next/head';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -10,6 +11,10 @@ import 'react-toastify/dist/ReactToastify.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
       <ToastContainer />
       <Web3Provider>
         <Component {...pageProps} />
